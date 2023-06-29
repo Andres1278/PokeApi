d47fa1eeae5cbf2ba2cad42d2cfbb9eecc174c4f
Pedro A. Villamil <pandres.villamil@hotmail.com>
2023-06-29 12:12:40 -0500

    Initial commit

---
+++ PokeApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeApi
+//
+//  Created by Pedro Andres Villamil on 29/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeApi/PokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiApp.swift
+//  PokeApi
+//
+//  Created by Pedro Andres Villamil on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
